c4d0f901b76c7ea16e986878fe3f818fdde02883
Ken Vilar <kenvilar@gmail.com>
2018-09-15 20:54:35 +0800

    Redirect to secret page if logged in successfully

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,8 @@
 const auth0 = require('auth0-js');
 require('dotenv').config();
 
+let tokens = {};
+
 const webAuth = new auth0.WebAuth({
 	domain: process.env.VUE_APP_AUTH0_DOMAIN,
 	clientID: process.env.VUE_APP_AUTH0_CLIENT_ID,
@@ -14,6 +16,18 @@ const login = () => {
 	webAuth.authorize();
 };
 
+const handleAuth = (callback) => {
+	webAuth.parseHash((err, authResult) => {
+		if (authResult && authResult.accessToken && authResult.idToken) {
+			tokens.accessToken = authResult.accessToken;
+      tokens.idToken = authResult.idToken;
+      callback();
+		} else {
+			console.log(err);
+		}
+	});
+}
+
 export default {
-	login
+	login, handleAuth
 }
--- src/views/Callback.vue
+++ src/views/Callback.vue
@@ -3,3 +3,25 @@
         <h1>Callbacking stuff...</h1>
     </div>
 </template>
+
+<script>
+import MY_AUTH from '../utils/auth';
+
+export default {
+  name: 'Callback',
+  beforeMount() {
+    MY_AUTH.handleAuth((err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      this.$router.push('/secret');
+    });
+  },
+  methods: {
+
+  }
+
+}
+</script>
