cd204d838cfa8d101cbee177345fa4f509e7ac2e
Ken Vilar <kenvilar@gmail.com>
2018-09-15 16:44:00 +0800

    Fix vue router; Reformat code

--- src/router.js
+++ src/router.js
@@ -8,35 +8,34 @@ import Callback from './views/Callback'
 
 Vue.use(Router)
 
-// noinspection JSAnnotator
 export default new Router({
-	mode: 'history',
-	base: process.env.BASE_URL,
-	routes: [
-		{
-			path: '/',
-			name: 'home',
-			component: Home
-		},
-		{
-			path: '/about',
-			name: 'about',
-			component: About
-		},
-		{
-			path: '/secret',
-			name: 'secret',
-			component: Secret
-		},
-		{
-			path: '/callback',
-			name: 'callback',
-			component: Callback
-		},
-		{
-			path: '/not-authorized',
-			name: 'not-authorized',
-			component: NotAuthorized
-		}
-	]
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/secret',
+      name: 'secret',
+      component: Secret
+    },
+    {
+      path: '/callback',
+      name: 'callback',
+      component: Callback
+    },
+    {
+      path: '/not-authorized',
+      name: 'not-authorized',
+      component: NotAuthorized
+    }
+  ]
 })
